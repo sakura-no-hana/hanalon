6a2ac9cd089950cebf03be68d0babb5b6a1f587c
eJi <veryeji@gmail.com>
2021-03-29 17:56:39 -0400

    Add seppuku command

--- src/cogs/diagnostics.py
+++ src/cogs/diagnostics.py
@@ -3,12 +3,12 @@ from discord.ext import commands
 
 from utils.access import is_dev
 
+
 class Diagnostics(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    @is_dev
     async def ping(self, ctx, precision='4'):
         e = discord.Embed(title='🏓 Pong!',
                           description=f'{("%." + precision + "f") % self.bot.latency} seconds!',
@@ -16,6 +16,18 @@ class Diagnostics(commands.Cog):
         e.set_footer(text=f'{ctx.author.name}#{ctx.author.discriminator}',
                      icon_url=ctx.author.avatar_url)
         await ctx.send(embed=e)
+
+    @commands.command()
+    @is_dev
+    async def seppuku(self, ctx):
+        e = discord.Embed(title='さよなら〜',
+                          timestamp=ctx.message.created_at, color=0xb9b6ed)
+        e.set_footer(text=f'{ctx.author.name}#{ctx.author.discriminator}',
+                     icon_url=ctx.author.avatar_url)
+        await ctx.send(embed=e)
+        await self.bot.change_presence(status=discord.Status.invisible)
+        await self.bot.logout()
+
 
 def setup(bot):
     bot.add_cog(Diagnostics(bot))
