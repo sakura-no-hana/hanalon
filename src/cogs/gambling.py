e7c62805bd6c61d04e67d97cc78327dd49ed2154
eJi <veryeji@gmail.com>
2021-04-02 14:58:18 -0400

    Add card draw commands

--- src/cogs/gambling.py
+++ src/cogs/gambling.py
@@ -1,3 +1,5 @@
+import random
+
 import d20
 import discord
 from discord.ext import commands, slash
@@ -7,6 +9,23 @@ from utils.responses import HanalonEmbed, HanalonResponse
 
 
 class Gambling(commands.Cog):
+    SUITS = ("♥", "♣", "♠", "♦", "🃏")
+    CARDS = (
+        "🇦",
+        "2️⃣",
+        "3️⃣",
+        "4️⃣",
+        "5️⃣",
+        "6️⃣",
+        "7️⃣",
+        "8️⃣",
+        "9️⃣",
+        "🔟",
+        "🇯",
+        "🇶",
+        "🇰",
+    )
+
     def __init__(self, bot):
         self.bot = bot
 
@@ -39,6 +58,62 @@ class Gambling(commands.Cog):
         await HanalonEmbed(
             title="Dice",
             description=str(result),
+            context=ctx,
+        ).respond()
+
+    @commands.command()
+    async def draw(self, ctx: commands.Context, joker: bool = False):
+        """
+        Draws a card.
+        """
+        if joker:
+            card = (
+                Gambling.CARDS[(value := random.randrange(54)) % 13]
+                + Gambling.SUITS[value // 13]
+            )
+            if (color := value // 13) == 5:
+                card = ("🟥" if color == 0 else "⬛") + card[1]
+        else:
+            card = (
+                Gambling.CARDS[(value := random.randrange(52)) % 13]
+                + Gambling.SUITS[value // 13]
+            )
+
+        await HanalonEmbed(
+            title="Cards",
+            description=card,
+            context=ctx,
+        ).respond(True)
+
+    @slash.cmd(name="draw")
+    async def _draw(
+        self,
+        ctx: slash.Context,
+        joker: slash.Option(
+            description="joker",
+            required=False,
+            type=slash.ApplicationCommandOptionType.BOOLEAN,
+        ) = False,
+    ):
+        """
+        Draws a card.
+        """
+        if joker:
+            card = (
+                Gambling.CARDS[(value := random.randrange(54)) % 13]
+                + Gambling.SUITS[value // 13]
+            )
+            if (color := value // 13) == 5:
+                card = ("🟥" if color == 0 else "⬛") + card[1]
+        else:
+            card = (
+                Gambling.CARDS[(value := random.randrange(52)) % 13]
+                + Gambling.SUITS[value // 13]
+            )
+
+        await HanalonEmbed(
+            title="Cards",
+            description=card,
             context=ctx,
         ).respond()
 
