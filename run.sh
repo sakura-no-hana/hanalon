ef6e1aa416ce8e3adb7c79b54d714697a52de201
eJi <veryeji@gmail.com>
2021-04-04 12:26:01 -0400

    Allow configurable image name in run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-docker rm $(docker stop $(docker ps -a -q --filter ancestor=hanalon --format="{{.ID}}"))
-docker build -t hanalon .
-base64 -in config.yaml | docker run --rm -e config=`xargs` hanalon
+NAME="hanalon/bot"
+
+docker rm $(docker stop $(docker ps -a -q --filter ancestor=$NAME --format="{{.ID}}"))
+docker build -t $NAME .
+base64 -in config.yaml | docker run --rm -e config=`xargs` $NAME
