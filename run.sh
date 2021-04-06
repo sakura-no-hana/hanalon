42bd3cd7254122c469ea48464eda6d9eb50c5eaf
eJi <veryeji@gmail.com>
2021-04-05 20:15:11 -0400

    Remove xargs from run.sh
    Apparently this is a bit finicky on some systems. I've changed it so that it should be more robust?

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ NAME="hanalon/bot"
 
 docker rm $(docker stop $(docker ps -a -q --filter ancestor=$NAME --format="{{.ID}}"))
 docker build -t $NAME .
-base64 -in config.yaml | docker run --rm -e config=`xargs` $NAME
+docker run --rm -e config=$(base64 -in config.yaml) $NAME
