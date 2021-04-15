9dacefa9e712f5f6c735402e13962428b5fa414f
eJi <veryeji@gmail.com>
2021-04-15 19:23:15 -0400

    Change render size in cog

--- src/cogs/rpg/move.py
+++ src/cogs/rpg/move.py
@@ -3,8 +3,6 @@ from discord.ext import commands
 from utils.rpg.game import Dungeon, InsufficientSpeed, Movement, ObstructedPath
 from utils.rpg.pieces import Being, Wall
 
-MAX_DIST = 3
-
 
 class GameAction(commands.Cog):
     def __init__(self, bot):
@@ -23,15 +21,15 @@ class GameAction(commands.Cog):
             ]
         )
 
-    def print_board(self) -> str:
-        board = self.dungeon.render(10, 10, self.chara.loc)
+    def board(self) -> str:
+        board = self.dungeon.render(13, 13, self.chara.loc)
         board.reverse()
         return "\n".join(["".join(a) for a in board])
 
     @commands.command()
     async def show(self, ctx) -> None:
         """Test command?"""
-        await ctx.send(f"{self.print_board()}")
+        await ctx.send(f"{self.board()}")
 
     @commands.command()
     async def move(self, ctx, delta_x: int, delta_y: int) -> None:
@@ -41,7 +39,7 @@ class GameAction(commands.Cog):
             await ctx.send("There's something in the way!")
         except InsufficientSpeed:
             await ctx.send("The destination is too far!")
-        await ctx.send(f"{self.print_board()}")
+        await ctx.send(f"{self.board()}")
 
 
 def setup(bot):
