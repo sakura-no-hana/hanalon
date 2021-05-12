f9d8affeab61f36e88dd50e6062b51ea772e7003
eJi <47072520+pyxiis@users.noreply.github.com>
2021-05-11 21:33:37 -0400

    Make BaseData __slots__ into a tuple
    This was a typo… oops

--- src/utils/rpg/db/base.py
+++ src/utils/rpg/db/base.py
@@ -2,7 +2,7 @@ from uuid import UUID
 
 
 class BaseData(object):
-    __slots__ = "identifier"
+    __slots__ = ("identifier",)
 
     def __init__(self, identifier: UUID):
         self.identifier = identifier
